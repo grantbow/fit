7f1c7352b32f5b9a0c7a59c501ae84c5c92ba01e
Grant Bowman <grantbow@gmail.com>
2018-11-27 16:47:48 -0800

    "initial no test errors"

---
+++ bugs/Config_test.go
@@ -0,0 +1,29 @@
+package bugs
+
+import (
+//	"testing"
+)
+
+//type tester struct {
+//	dir string
+//	config *Config
+//}
+
+/*
+func (t *tester) Setup() {
+}
+func (t *tester) Teardown() {
+}
+
+func TestRead(t *testing.T) {
+}
+
+func TestDir(t *testing.T) {
+}
+
+func TestDefaultDescriptionFile(t *testing.T) {
+}
+
+func TestPMIT(t *testing.T) {
+}
+*/
