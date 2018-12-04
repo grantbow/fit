3e9710b72a07615dc77a6adee001c0fc25eb815b
Grant Bowman <grantbow@gmail.com>
2018-12-03 21:25:24 -0800

    config redone BugDir

--- bugs/Config.go
+++ bugs/Config.go
@@ -9,11 +9,15 @@ import (
 )
 
 type Config struct {
-	Dir                    Directory // location of root dir and .bug.yml
-	PMIT                   string    // overrides auto-find root dir
-	                                 // overridden by PMIT environment variable
-	DefaultDescriptionFile string    // new bug Description text template
-	ImportXmlDump          bool      // saves raw json files of import
+	// storage of location of root dir with .bug.yml
+		// if we are reading the config file we already found the root
+	    // auto-find root dir or
+	    // overridden by PMIT environment variable
+	BugDir                 string `json:"BugDir"`
+	// new bug Description text template
+	DefaultDescriptionFile string `json:"DefaultDescriptionFile"`
+	// saves raw json files of import
+	ImportXmlDump          bool   `json:"ImportXmlDump"`
 }
 
 var NoConfigError = errors.New("No .bug.yml provided")
@@ -27,14 +31,14 @@ func check(e error) () {
 }
 
 func (c *Config) Read() (err error) {
-	dat, err := ioutil.ReadFile(string(c.Dir)+"/.bug.yml")
+	dat, err := ioutil.ReadFile(string(c.BugDir)+"/.bug.yml")
 	check(err)
 	err = yaml.Unmarshal(dat, &c)
 	check(err)
-    env := os.Getenv("PMIT")
-    if env != "" {
-        c.PMIT = env
-    }
+    //env := os.Getenv("PMIT")
+    //if env != "" {
+    //    c.BugDir = env
+    //}
 
 	return nil
 }
